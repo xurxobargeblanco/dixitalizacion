cafedffddd1566f0f7a0c3d8c018e16e45e703fc
xurxobargeblanco <xurxo.barge.blanco@iessanmamede.com>
2026-09-15 12:00:44 +0200

    Desarrollo UD1_T01.py completado

--- UD1/UD1_T01/UD1_T01.py
+++ UD1/UD1_T01/UD1_T01.py
@@ -1,5 +1,9 @@
+# Conversor de temperaturas
+
 temperatura = int(input("Introduce una temperatura en grados Celsius"))
+
 tempF = (temperatura * 9/5) +32
+tempK = temperatura + 273.15
 
-
-print(f"La temperaura en Fahrenheit es {tempF}")
+print(f"La temperaura en Fahrenheit es {tempF} ºF")
+print(f"La temperaura en Keelvin es {tempK} K")
