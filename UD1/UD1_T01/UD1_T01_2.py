3dc20338fe0312b3f3cfce748e2b783b67db57de
xurxobargeblanco <xurxo.barge.blanco@iessanmamede.com>
2026-09-17 10:13:10 +0200

    Realizando ejercicio UD1_T01.py

--- UD1/UD1_T01/UD1_T01_2.py
+++ UD1/UD1_T01/UD1_T01_2.py
@@ -1,2 +1,14 @@
 # Números pares e impares
 
+lista = str(input("Introduce una serie de numeros pares e impares separados por espacios"))
+numeros = lista.split(" ")
+lista_numeros_pares = []
+lista_numeros_inpares = []
+
+for c in numeros:
+    if c % 2 == 0 :
+       lista_numeros_pares.append(int(c)) 
+    else :
+        lista_numeros_inpares.append(int(c)) 
+
+    
