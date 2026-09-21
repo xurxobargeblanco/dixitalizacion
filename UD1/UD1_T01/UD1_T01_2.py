36a7698519a84b5e9409688924b7bde610874bfa
xurxobargeblanco <xurxo.barge.blanco@iessanmamede.com>
2026-09-21 09:55:57 +0200

    Exercicio 2 correxido en clase

--- UD1/UD1_T01/UD1_T01_2.py
+++ UD1/UD1_T01/UD1_T01_2.py
@@ -1,6 +1,6 @@
 # Números pares e impares
 
-numeros = map(int, input("Introduce una serie de numeros pares e impares separados por espacios").split())
+numeros = list(map(int, input("Introduce una serie de numeros pares e impares separados por espacios").split()))
 
 lista_numeros_pares = [n for n in numeros if n%2 == 0]
 lista_numeros_inpares = [n for n in numeros if n%2 != 0]
