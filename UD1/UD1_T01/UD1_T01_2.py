1ec37bc120d8ec6b67cc2cfff488c451a06cecb7
xurxobargeblanco <xurxo.barge.blanco@iessanmamede.com>
2026-09-21 09:53:23 +0200

    Exercicio 2 adaptado a programación en Python

--- UD1/UD1_T01/UD1_T01_2.py
+++ UD1/UD1_T01/UD1_T01_2.py
@@ -1,14 +1,9 @@
 # Números pares e impares
 
-lista = str(input("Introduce una serie de numeros pares e impares separados por espacios"))
-numeros = lista.split(" ")
-lista_numeros_pares = []
-lista_numeros_inpares = []
+numeros = map(int, input("Introduce una serie de numeros pares e impares separados por espacios").split())
 
-for c in numeros:
-    if c % 2 == 0 :
-       lista_numeros_pares.append(int(c)) 
-    else :
-        lista_numeros_inpares.append(int(c)) 
+lista_numeros_pares = [n for n in numeros if n%2 == 0]
+lista_numeros_inpares = [n for n in numeros if n%2 != 0]
 
-    
+print(f"Pares : {lista_numeros_pares}")
+print(f"Inpares : {lista_numeros_inpares}")
